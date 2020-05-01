210b478897c724d69944b938693fedac7041c0f7
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-05-01 18:49:09 +0200

    Started to script the project

---
+++ main.py
@@ -0,0 +1,102 @@
+import numpy as np
+
+import sys
+import pickle
+import string
+import scipy
+
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.base import BaseEstimator
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier
+
+from skmultilearn.problem_transform import ClassifierChain, LabelPowerset
+
+from text import PreProcessing
+
+# import tensorflow as tf
+# import tensorflow.keras.backend as K
+# import tensorflow.keras.utils
+# import tensorflow.keras.callbacks
+# from tensorflow.python.client import device_lib
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Dropout, Embedding, Flatten, Conv1D, GlobalMaxPooling1D
+# from tensorflow.keras.callbacks import EarlyStopping
+
+def load_file(path):
+    with open(path, 'rb') as file:
+        return pickle.load(file)
+
+def make_prediction(model, vect, text):
+    tokenized = PreProcessing().cleanText(text)
+
+    if vect != None:
+        X = vect.transform([tokenized])
+    else:
+        X = np.array([tokenized])
+
+    pred = model.predict(X)
+
+    if vect != None:
+        with open('models/mlb.pk', 'rb') as file:
+            mlb = pickle.load(file)
+            print(mlb.inverse_transform(pred))
+    else:
+        print(pred)
+    
+
+if __name__ == '__main__':
+    msg = """
+    Welcome to the genie! Remember, you have only
+    3 wishes. Choose your prediction model carefully.
+
+    1. [R]andom Forest
+    2. [K]NN
+    3. [N]eural Network
+    4. [S]upport Vector Machine
+    5. [M]atrix Regression
+
+    Write [E] if you are not sure and you need time to think.\n
+    Enter your wish: """
+
+    while True:
+        action = input(msg).upper()
+
+        if action == 'E':
+            break
+        if action not in "RKNSM" or len(action) != 1:
+            print("I don't know how to do that")
+            continue
+
+        text = input("""
+        Enter the text of the event you want to predict: """)
+
+        if action == 'R':
+            model = load_file('models/model_rf.pk')
+            vect = load_file('models/bow_vectorizer.pk')
+
+            make_prediction(model, vect, text)
+            break
+        elif action == 'K':
+            model = load_file('models/model_knn.pk')
+            vect = load_file('models/bow_vectorizer.pk')
+
+            make_prediction(model, vect, text)
+            break
+        elif action == 'N':
+            print('Not yet implemented')
+            break
+        elif action == 'S':
+            model = load_file('models/model_svm.pk')
+            vect = load_file('models/bow_vectorizer.pk')
+
+            make_prediction(model, vect, text)
+            break
+        elif action == 'M':
+            model = load_file('models/model_mr.pk')
+            vect = load_file('models/bow_vectorizer.pk')
+
+            make_prediction(model, None, text)
+            break
---
+++ text.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import nltk
+import string
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+
+class PreProcessing():
+    def __init__(self, file=None):
+        self.file = file
+    
+    def preprocess(self):
+        df = pd.read_csv(self.file, encoding="utf-8")
+        df["cleaned_desc"] = df["description"].apply(self.cleanText)
+        df["cleaned_title"] = df["title"].apply(self.cleanText)
+        df["full_cleaned"] = df["cleaned_desc"] + df["cleaned_title"]
+        df = df.reset_index()
+
+        return df["full_cleaned"].values
+
+
+    def cleanText(self, x):
+        soup = BeautifulSoup(x, 'lxml')
+        no_html_text = soup.get_text()
+        tokens = nltk.word_tokenize(no_html_text)
+        tokens = [w.lower() for w in tokens]
+        words = [word for word in tokens if word.isalpha()]
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        stopwords = nltk.corpus.stopwords.words('italian')
+        stopwords.extend(string.punctuation)
+        stopwords.extend(nltk.corpus.stopwords.words('english'))
+        words = [w for w in stripped if w.isalpha() and not w in stopwords]
+    
+        return " ".join(words)  
